67f1d5276eb8223ae9e67f0567939f3dc19ecd99
DEADBYTE <termay10000@gmail.com>
2026-06-19 16:32:26 +0700

    add audit front command

--- src/deadgl_front.c
+++ src/deadgl_front.c
@@ -2,4 +2,4 @@
 #include <string.h>
 int deadgl_render_main(int argc,char**argv);
 int dgl_inspect_file(const char*path);
-int main(int argc,char**argv){if(argc==3&&strcmp(argv[1],"inspect")==0){return dgl_inspect_file(argv[2]);}return deadgl_render_main(argc,argv);}
+int main(int argc,char**argv){if(argc==3&&(strcmp(argv[1],"inspect")==0||strcmp(argv[1],"audit")==0)){return dgl_inspect_file(argv[2]);}return deadgl_render_main(argc,argv);}
