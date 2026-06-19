27b12691099180c17dc750e83f1a95be959d742e
DEADBYTE <termay10000@gmail.com>
2026-06-19 12:58:39 +0700

    add front

---
+++ src/deadgl_front.c
@@ -0,0 +1,5 @@
+#include "deadgl.h"
+#include <string.h>
+int deadgl_render_main(int argc,char**argv);
+int dgl_inspect_file(const char*path);
+int main(int argc,char**argv){if(argc==3&&strcmp(argv[1],"inspect")==0){return dgl_inspect_file(argv[2]);}return deadgl_render_main(argc,argv);}
