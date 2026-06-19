e8acdd65cb57047f0ac2440d0d1e74787a872215
DEADBYTE <termay10000@gmail.com>
2026-06-19 11:29:45 +0700

    start v1.3 core surface API

--- include/deadgl.h
+++ include/deadgl.h
@@ -8,7 +8,7 @@
 extern "C" {
 #endif
 
-#define DGL_VERSION "1.2.0"
+#define DGL_VERSION "1.3.0"
 #define DGL_MAX_DIM 16384
 #define DGL_MAX_PIXELS 67108864u
 #define DGL_FAR 1.0e30f
@@ -53,12 +53,16 @@ uint32_t dgl_get_pixel(const DGL_Surface *s, int x, int y);
 float dgl_get_depth(const DGL_Surface *s, int x, int y);
 
 void dgl_line(DGL_Surface *s, int x0, int y0, int x1, int y1, uint32_t color);
+void dgl_depth_line(DGL_Surface *s, DGL_Vertex a, DGL_Vertex b, uint32_t color);
 void dgl_rect(DGL_Surface *s, int x, int y, int w, int h, uint32_t color);
 void dgl_fill_rect(DGL_Surface *s, int x, int y, int w, int h, uint32_t color);
 void dgl_circle(DGL_Surface *s, int cx, int cy, int r, uint32_t color);
 void dgl_fill_circle(DGL_Surface *s, int cx, int cy, int r, uint32_t color);
 void dgl_wire_tri(DGL_Surface *s, DGL_Vertex a, DGL_Vertex b, DGL_Vertex c, uint32_t color);
+void dgl_wire_tri_depth(DGL_Surface *s, DGL_Vertex a, DGL_Vertex b, DGL_Vertex c, uint32_t color);
 void dgl_tri(DGL_Surface *s, DGL_Vertex a, DGL_Vertex b, DGL_Vertex c);
+void dgl_quad(DGL_Surface *s, DGL_Vertex a, DGL_Vertex b, DGL_Vertex c, DGL_Vertex d);
+void dgl_wire_quad_depth(DGL_Surface *s, DGL_Vertex a, DGL_Vertex b, DGL_Vertex c, DGL_Vertex d, uint32_t color);
 
 uint64_t dgl_hash(const DGL_Surface *s);
 int dgl_save_ppm(const DGL_Surface *s, const char *path);
