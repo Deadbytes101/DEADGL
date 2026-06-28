a3018e4ce67f627d7c5354c0e64cfe15dda11d56
DEADBYTE <termay10000@gmail.com>
2026-06-29 01:13:12 +0700

    resolve golden binary path

--- scripts/golden.sh
+++ scripts/golden.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -eu
 
+DEADGL=${DEADGL:-}
+if [ -z "$DEADGL" ]; then
+    if [ -f ./build/deadgl ]; then
+        DEADGL=./build/deadgl
+    elif [ -f ./build/deadgl.exe ]; then
+        DEADGL=./build/deadgl.exe
+    else
+        echo "missing build/deadgl; run make first" >&2
+        exit 1
+    fi
+fi
+
 need_hash() {
     name=$1
     actual=$2
@@ -14,18 +26,13 @@ need_hash() {
 
 scene_hash() {
     scene=$1
-    ./build/deadgl hash "$scene" | awk '{print $1}'
+    "$DEADGL" hash "$scene" | awk '{print $1}'
 }
 
 demo_hash() {
     name=$1
-    ./build/deadgl demo "$name" -o "build/golden_$name.ppm" | awk '/^hash / {print $2}'
+    "$DEADGL" demo "$name" -o "build/golden_$name.ppm" | awk '/^hash / {print $2}'
 }
-
-if [ ! -f ./build/deadgl ]; then
-    echo "missing build/deadgl; run make first" >&2
-    exit 1
-fi
 
 need_hash cube "$(demo_hash cube)" 95689452fd3b5017
 need_hash command_machine "$(scene_hash examples/command_machine.dgl)" 7dd8eb60b1510af7
